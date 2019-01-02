09bedce872f62c14984e48935e7d3f807cc5f0fa
programmiri <programmiri@gmail.com>
2019-01-02 17:36:12 +0100

    Format again bc some files still used spaces instead of tabs

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -2,25 +2,25 @@
 package raindrops
 
 import (
-        "strconv"
-        "strings"
+	"strconv"
+	"strings"
 )
 
 // Convert converts a number to a string in raindrop-speak
 func Convert(num int) string {
-        var r []string
-        if num%3 == 0 {
-                r = append(r, "Pling")
-        }
-        if num%5 == 0 {
-                r = append(r, "Plang")
-        }
-        if num%7 == 0 {
-                r = append(r, "Plong")
-        }
+	var r []string
+	if num%3 == 0 {
+		r = append(r, "Pling")
+	}
+	if num%5 == 0 {
+		r = append(r, "Plang")
+	}
+	if num%7 == 0 {
+		r = append(r, "Plong")
+	}
 
-        if len(r) == 0 {
-                r = append(r, strconv.Itoa(num))
-        }
-        return strings.Join(r, "")
+	if len(r) == 0 {
+		r = append(r, strconv.Itoa(num))
+	}
+	return strings.Join(r, "")
 }
--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -4,8 +4,8 @@ package twofer
 //ShareWith creates a sentence that uses a given name
 //or, if that is not given, "you"
 func ShareWith(name string) string {
-        if name == "" {
-                name = "you"
-        }
-        return "One for " + name + ", one for me."
+	if name == "" {
+		name = "you"
+	}
+	return "One for " + name + ", one for me."
 }
