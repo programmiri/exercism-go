1104f78642dace0ac6fd489a3c3c0200841fe6e7
programmiri <programmiri@gmail.com>
2019-01-03 20:22:56 +0100

    Solve excercise "Proverb"

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,15 +1,17 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb implements a method to create a proverb
 package proverb
 
-// Proverb should have a comment documenting it.
+// Proverb takes a input and creates a proverb in a special manner with it.
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	var s = []string{}
+
+	if len(rhyme) == 0 {
+		return s
+	}
+
+	for i := 0; i < len(rhyme)-1; i++ {
+		s = append(s, "For want of a "+rhyme[i]+" the "+rhyme[i+1]+" was lost.")
+	}
+	return append(s, "And all for the want of a "+rhyme[0]+".")
+
 }
