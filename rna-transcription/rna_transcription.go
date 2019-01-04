8b937c6be1142e611d61b6652481e2834c8611e1
programmiri <programmiri@gmail.com>
2019-01-04 19:47:20 +0100

    Solve side-excercise "RNA transcription"

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,14 @@
+// Package strand implements a method to extrakt information from a DNA strand
 package strand
 
+import "strings"
+
+// ToRNA return its RNA complement from a given DNA strand
 func ToRNA(dna string) string {
+	r := strings.NewReplacer("G", "C",
+		"C", "G",
+		"T", "A",
+		"A", "U")
+	return r.Replace(dna)
+
 }
