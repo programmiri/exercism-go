8975f15202015d7bcd84ee4f1626bac9155ad41f
programmiri <programmiri@gmail.com>
2019-01-05 20:55:12 +0100

    Solve side-excercise "Nucleotide Count"

--- nucleotide-count/nucleotide_count.go
+++ nucleotide-count/nucleotide_count.go
@@ -1,11 +1,26 @@
+// Package dna implements a method to extrakt information from a DNA strand
 package dna
 
-// Histogram is a mapping from nucleotide to its count in given DNA.
-// Choose a suitable data type.
-type Histogram
+import (
+	"errors"
+	"strings"
+)
 
-// DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]int
+
+// DNA is a list of nucleotides.
+type DNA string
+
+func validNuc(d DNA) bool {
+	for _, r := range d {
+		if r != 'A' && r != 'C' && r != 'G' && r != 'T' {
+			return false
+		}
+	}
+	return true
+
+}
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
@@ -15,5 +30,16 @@ type DNA
 // Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
 	var h Histogram
+	if len(d) > 0 && !validNuc(d) {
+		return h, errors.New("ivalid nucleotides")
+	}
+	nucs := [4]rune{'A', 'C', 'G', 'T'}
+	h = make(map[rune]int)
+	for _, r := range nucs {
+
+		h[r] = strings.Count(string(d), string(r))
+	}
 	return h, nil
 }
+
+//
