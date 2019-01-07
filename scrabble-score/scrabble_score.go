4774f958f9096e2c6afa565d0eda797802e68830
programmiri <programmiri@gmail.com>
2019-01-07 20:43:48 +0100

    Optimize excercise 'Scrabble Score'

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,27 +1,27 @@
 // Package scrabble implements a scrabble game
 package scrabble
 
-import "strings"
+import (
+	"unicode"
+)
+
+var letterValue = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
+	'L': 1, 'N': 1, 'R': 1, 'S': 1,
+	'T': 1, 'D': 2, 'G': 2, 'B': 3,
+	'C': 3, 'M': 3, 'P': 3, 'F': 4,
+	'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5, 'J': 8, 'X': 8, 'Q': 10,
+	'Z': 10,
+}
 
 // Score calculates the scrabble score for a given word
 func Score(s string) int {
-	letterValue := map[int][]string{
-		1:  {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"},
-		2:  {"D", "G"},
-		3:  {"B", "C", "M", "P"},
-		4:  {"F", "H", "V", "W", "Y"},
-		5:  {"K"},
-		8:  {"J", "X"},
-		10: {"Q", "Z"},
-	}
+	r := []rune(s)
 	count := 0
-	for k, v := range letterValue {
-		currCount := 0
-		for i := 0; i < len(v); i++ {
-			currScore := strings.Count(strings.ToUpper(s), v[i]) * k
-			currCount = currCount + currScore
-		}
-		count = count + currCount
+	for i := 0; i < len(r); i++ {
+		count += letterValue[unicode.ToUpper(r[i])]
+
 	}
 	return count
 }
