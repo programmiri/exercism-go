eede8c1de3a0468db842bfad22752d184dfa9e87
programmiri <programmiri@gmail.com>
2019-01-08 20:08:21 +0100

    Optimize excercise 'Scrabble Score' further

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -2,26 +2,24 @@
 package scrabble
 
 import (
-	"unicode"
+	"strings"
 )
 
 var letterValue = map[rune]int{
 	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
-	'L': 1, 'N': 1, 'R': 1, 'S': 1,
-	'T': 1, 'D': 2, 'G': 2, 'B': 3,
-	'C': 3, 'M': 3, 'P': 3, 'F': 4,
-	'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-	'K': 5, 'J': 8, 'X': 8, 'Q': 10,
+	'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3,
+	'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4,
+	'Y': 4, 'K': 5, 'J': 8, 'X': 8, 'Q': 10,
 	'Z': 10,
 }
 
 // Score calculates the scrabble score for a given word
 func Score(s string) int {
-	r := []rune(s)
 	count := 0
-	for i := 0; i < len(r); i++ {
-		count += letterValue[unicode.ToUpper(r[i])]
-
+	for _, r := range strings.ToUpper(s) {
+		count += letterValue[r]
 	}
+
 	return count
 }
