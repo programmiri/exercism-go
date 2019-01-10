959b77f13f665cd0fdfd353b8bc0048c5543d893
programmiri <programmiri@gmail.com>
2019-01-10 20:49:46 +0100

    Solve side-excercise "Reverse string" and it feels like cheating bc it's the top answer on SO. 🙈

---
+++ reverse-string/reverse_string.go
@@ -0,0 +1,11 @@
+// Package reverse implements a method to reverse things
+package reverse
+
+// String takes a string and returns it reversed
+func String(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
